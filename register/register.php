ae912e8d223b7c6ec09dc584cd3fcb6613b3e49a
coutin.dev <rycharddasilvacouto2@gmail.com>
2024-08-09 11:53:59 -0300

    ultima alteracao insercao

--- register/register.php
+++ register/register.php
@@ -1,9 +1,10 @@
 <?php
 require("../connect/connect.php");
+
 $registerusername = $_POST['registerusername'];
 $registertelephone = $_POST['registertelephone'];
 $registeremail = $_POST['registeremail'];
-$registrasenha = $_POST['registrasenha'];
+$registrasenha = $_POST['registersenha'];
 $cpf = $_POST['cpf'];
 $nomeCompleto = $_POST['nomeCompleto'];
 $dataNasc = $_POST['dataNasc'];
@@ -13,10 +14,14 @@ $nomeSocial = $_POST['nomeSocial'];
 $numeroendereco = $_POST['numeroendereco'];
 $complementoendereco = $_POST['complementoendereco'];
 
-$stmt = $conn->prepare("INSERT INTO usuario (nome, telefone, email, senha, numeroendereco, complementoendereco) VALUES (?,?,?,?,?,?");
+$stmt = $conn->prepare("INSERT INTO usuario (nome, telefone, email, senha, numeroendereco, complementoendereco) VALUES (?,?,?,?,?,?)");
 $stmt->bind_param("ssssis", $registerusername, $registertelephone, $registeremail, $registrasenha, $numeroendereco, $complementoendereco);
+
 if($stmt->execute()){
     $stmt->close();
     $conn->close();
+    echo "<script language='javascript' type='text/javascript'>
+    alert('Usuário cadastrado com sucesso!');window.location.href='login.html';</script>";
 }
+
 ?>
