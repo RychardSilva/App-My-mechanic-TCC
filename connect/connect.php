02b592a991d68b28c025e1be9c4c1d41a7194fcd
coutin.dev <rycharddasilvacouto2@gmail.com>
2024-08-09 11:24:08 -0300

    conexao banco de dados

---
+++ connect/connect.php
@@ -0,0 +1,16 @@
+<?php
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $bd = 'my_mechanic';
+    $port = 3307;
+
+    $conn = mysqli_connect($host, $user, $password, $bd, $port);
+
+    if(!$conn){
+        echo 'Erro ao conectar ao banco de dados';
+    }
+    else{
+        echo 'Conexão efetuada realizada com sucesso';
+    }
+?>
---
+++ register/register.php
@@ -0,0 +1,22 @@
+<?php
+require("../connect/connect.php");
+$registerusername = $_POST['registerusername'];
+$registertelephone = $_POST['registertelephone'];
+$registeremail = $_POST['registeremail'];
+$registrasenha = $_POST['registrasenha'];
+$cpf = $_POST['cpf'];
+$nomeCompleto = $_POST['nomeCompleto'];
+$dataNasc = $_POST['dataNasc'];
+$cnpj = $_POST['cnpj'];
+$razaoSocial = $_POST['razaoSocial'];
+$nomeSocial = $_POST['nomeSocial'];
+$numeroendereco = $_POST['numeroendereco'];
+$complementoendereco = $_POST['complementoendereco'];
+
+$stmt = $conn->prepare("INSERT INTO usuario (nome, telefone, email, senha, numeroendereco, complementoendereco) VALUES (?,?,?,?,?,?");
+$stmt->bind_param("ssssis", $registerusername, $registertelephone, $registeremail, $registrasenha, $numeroendereco, $complementoendereco);
+if($stmt->execute()){
+    $stmt->close();
+    $conn->close();
+}
+?>
